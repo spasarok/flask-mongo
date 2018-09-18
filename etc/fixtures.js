4223f9ada4d0024b48ae696c7249839e0ba64240
Kimberly Spasaro <kimberly.spasaro@target.com>
2018-09-18 12:47:38 -0500

    first commit

---
+++ etc/fixtures.js
@@ -0,0 +1,33 @@
+db.users.insert([
+  {
+    "_id": "z003f37",
+    "name": "Kim"
+  },
+  {
+    "_id": "123",
+    "name": "Marion"
+  },
+  {
+    "_id": "456",
+    "name": "Kelly"
+  },
+  {
+    "_id": "789",
+    "name": "Wako"
+  }
+])
+
+db.stores.insert([
+  {
+    "_id": 1,
+    "name": "Target"
+  },
+  {
+    "_id": 2,
+    "name": "Amazon"
+  },
+  {
+    "_id": 3,
+    "name": "Walmart"
+  }
+])
