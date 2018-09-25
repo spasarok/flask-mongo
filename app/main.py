2fe1bd1c5b114a974591877ab71b58ef8acfef39
Kimberly Spasaro <kimberly.spasaro@target.com>
2018-09-25 13:32:31 -0500

    add get requests for stores and users

--- app/main.py
+++ app/main.py
@@ -1,9 +1,34 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import json
+
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://root:root@localhost:4000/test_db?authSource=admin"
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/stores')
+def get_stores():
+    result = mongo.db.stores.find()
+    result_str = json.dumps(list(result))
+    return '"stores": {}'.format(result_str)
+
+@app.route('/stores/id/<int:id>')
+def get_stores_by_id(id):
+    result = mongo.db.stores.find({"_id": id})
+    result_str = json.dumps(list(result))
+    return '"stores": {}'.format(result_str)
+
+@app.route('/users')
+def get_users():
+    result = mongo.db.users.find()
+    result_str = json.dumps(list(result))
+    return '"users": {}'.format(result_str)
 
 if __name__ == '__main__':
     app.run()
